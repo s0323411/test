22a77cdb606330695f7d5c4d92a33e27558f3e96
s0323411 <s0323411@outlook.com>
2024-06-24 12:02:54 +0000

    Update run1.ps1

--- .github/workflows/run1.ps1
+++ .github/workflows/run1.ps1
@@ -1,4 +1,4 @@
 $URL = "https://www.youtube.com/watch?v=UK81GCw1KE8"
-$stream = yt-dlp -g $URL
+$stream = .\yt-dlp.exe -g $URL
 "#EXTINF:-1," | Out-File -FilePath "playlist.m3u" -Encoding UTF8
 $stream | Out-File -Append -FilePath "playlist.m3u" -Encoding UTF8
